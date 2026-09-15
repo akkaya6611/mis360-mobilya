e567dddc1f82a624dbbfa8441aaa3c8e2fb00b37
akkaya6611 <132543380+akkaya6611@users.noreply.github.com>
2026-09-15 23:54:48 +0300

    v1.9.3: Footer Copyright Heart & Updates Release

---
+++ page-yardim-merkezi.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template for /yardim-merkezi/ page
+ *
+ * @package Mis360-Mobilya
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+require_once __DIR__ . '/page-help-center.php';
